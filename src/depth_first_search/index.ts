947cca9484222842540069213cba81821047b35f
城間凌 <ryo.shiroma@broadleaf.co.jp>
2025-06-09 17:20:19 +0900

    second commit

--- src/depth_first_search/index.ts
+++ src/depth_first_search/index.ts
@@ -1,3 +1,4 @@
+
 export interface FileSystemNode {
   name: string;
   isDirectory: boolean;
@@ -8,4 +9,14 @@ export const depthFirstSearch = (
   root: FileSystemNode,
   target: string,
 ): boolean => {
+  let nameList = [];
+ 
+  for(let i =0; i < root.children.length; i++ ){
+    if(root.children[i]){
+      nameList.push(root.children[i].name);
+      console.log(nameList);
+      depthFirstSearch(root.children[i],target);
+    }
+  }
+  console.log(nameList);
 };
